bb521d31ad75dcce310c08f46460ca8bd3602a24
cristian.pardo <cristian.pardo@gmail.com>
2024-02-21 13:01:41 -0500

    commit node

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [nombre, setNombre]= useState ("");
+  const [edad, setEdad]= useState (0);
+  const [pais, setPais]= useState ("");
+  const [cargo, setCargo]= useState ("");
+  const [anios, setAnios]= useState (0);
+
+  const mostrarDatos = () =>{
+    alert(nombre);
+  }
+
   return (
     <div className="App">
-      div
+    <div className='datos'>
+      <label>Nombre:<input 
+      onChange={(event) => {
+        setNombre(event.target.value);
+      }}
+      type="text" id="" name="" placeholder="" /></label>
+      <label>Edad:<input 
+      onChange={(event ) =>{
+        setEdad(event.target.value);
+      }}
+      type="number" id="" name="" placeholder="" /></label>
+      <label>Pais:<input 
+      onChange={(event) => {
+        setPais(event.target.value);
+      }}
+      type="text" id="" name="" placeholder="" /></label>
+      <label>Cargo:<input 
+      onChange={(event) => {
+        setCargo(event.target.value);
+      }}
+      type="text" id="" name="" placeholder="" /></label>
+      <label>Años:<input 
+      onChange={(event) => {
+        setAnios(event.target.value);
+      }}
+      type="numer" id="" name="" placeholder="" /></label>
+      <button onClick={mostrarDatos}>Registrar</button>
+    </div>
     </div>
   );
 }
